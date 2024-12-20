2c1179fcef7de293d33223f37cd2df6184d33efc
gab2691 <gabriel_bartholo_batista@hotmail.com>
2024-12-20 09:48:26 -0300

    first commit

---
+++ src/main/java/com/br/emailService/controller/EmailController.java
@@ -0,0 +1,27 @@
+package com.br.emailService.controller;
+
+import com.br.emailService.dto.EmailRequestDTO;
+import com.br.emailService.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequestDTO emailRequest) {
+        try {
+            emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getMessage());
+            return ResponseEntity.ok("Email sent successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email: " + e.getMessage());
+        }
+    }
+}
+
